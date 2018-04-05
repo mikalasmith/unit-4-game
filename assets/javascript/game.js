fdc11e6369e43ad182caa868619d0418cfab9d50
Mikala Smith <mikalasmith@mikalas-air.home>
2018-04-05 16:13:03 -0400

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,3 @@
-
-
-     $( document ).ready(function() {
-         console.log( "document loaded" );
-     });
-  
-     $( window ).on( "load", function() {
-         console.log( "window loaded" );
-        resetRound();
-     });
-
-
-
-
 //define all variables
 var goalNum;
 var wins = 0;
@@ -20,53 +6,211 @@ var redGem;
 var yellowGem;
 var clearGem;
 var greenGem;
-var userSum = 0;
-
+var userSum ;
 
 // define all functions
-
+$( document ).ready(function() {
 function randomNum(min,max) {
     return Math.floor(Math.random()*((max-min)+1)+min) 
 }
 
 //yellowGem = randomNum(1,12);
 //console.log(yellowGem);
-
+//function resetRound() {
+    //redGem = randomNum(1,12);
+    //yellowGem = randomNum(1,12);
+    //clearGem = randomNum(1,12);
+    //greenGem = randomNum(1,12);
+   // goalNum = randomNum (19, 120);
+    //userSum = 0; 
+    //console.log(redGem, yellowGem, clearGem, greenGem, goalNum);
+//}
+//resetRound();
 
 function checkStatus() {
     if (userSum === goalNum){
     wins++;
-    resetRound();
-} else if (userSum > goalNum) {
+    console.log(wins)
+    resetRound();      
+} else if(userSum > goalNum){
     losses++;
+    console.log(losses)
     resetRound();
-}
-console.log("checkstatus");
+}  
+
 console.log("user sum="+ userSum);
 }
 
+function newGame(){
+    goalNum = randomNum(19,120);
+    $ ("#goal").text(goalNum);
+    redGem =0;
+    yellowGem =0;
+    clearGem=0;
+    greenGem=0;
+}
 // funtion that will reset each round of the game, without refreshing the page.
-function resetRound() {
-    redGem = randomNum(1,12);
-    yellowGem = randomNum(1,12);
-    clearGem = randomNum(1,12);
-    greenGem = randomNum(1,12);
-    goalNum = randomNum (19, 120);
-    userSum = 0; 
-    console.log("resetround");
-}
 
-resetRound();
 
-function gemClick (gem) {
-    userSum =+ gem;
-    checkStatus();
-    console.log("gemclick")
-}
+// function gemClick (gem) {
+//     console.log()
+//     userSum =+ gem;
+//     alert("gem clicked"+gem)
+//     console.log(userSum);
+//     checkStatus();
+//     console.log("gemclick")
+
+// }function gemClick (gem) {
+//     console.log()
+//     userSum =+ gem;
+//     alert("gem clicked"+gem)
+//     console.log(userSum);
+//     checkStatus();
+//     console.log("gemclick")
+// }
+
 
 //click events
-console.log(yellowGem);
- $ ("button").on("click", "#yellow", gemClick(yellowGem));
+
+    $("#yellow").on("click",function(){
+        console.log('hi')
+if(yellowGem===0){
+    yellowGem = randomNum(1,12);
+    $("#yellow").text($(this).text());
+    console.log(yellowGem);
+}
+userSum+=yellowGem;
+$("#sum").text(userSum);
+    });
+
+
+      
+         
+
+
+
+
+
+
+
+ });
+  
+
+
+
+
+
+
+
+
+
+
+//when click on gem
+//define all variables
+var goalNum;
+var wins = 0;
+var losses = 0;
+var redGem;
+var yellowGem;
+var clearGem;
+var greenGem;
+var userSum ;
+
+// define all functions
+$( document ).ready(function() {
+function randomNum(min,max) {
+    return Math.floor(Math.random()*((max-min)+1)+min) 
+}
+
+//yellowGem = randomNum(1,12);
+//console.log(yellowGem);
+//function resetRound() {
+    //redGem = randomNum(1,12);
+    //yellowGem = randomNum(1,12);
+    //clearGem = randomNum(1,12);
+    //greenGem = randomNum(1,12);
+   // goalNum = randomNum (19, 120);
+    //userSum = 0; 
+    //console.log(redGem, yellowGem, clearGem, greenGem, goalNum);
+//}
+//resetRound();
+
+function checkStatus() {
+    if (userSum === goalNum){
+    wins++;
+    console.log(wins)
+    resetRound();      
+} else if(userSum > goalNum){
+    losses++;
+    console.log(losses)
+    resetRound();
+}  
+
+console.log("user sum="+ userSum);
+}
+
+function newGame(){
+    goalNum = randomNum(19,120);
+    $ ("#goal").text(goalNum);
+    redGem =0;
+    yellowGem =0;
+    clearGem=0;
+    greenGem=0;
+}
+// funtion that will reset each round of the game, without refreshing the page.
+
+
+// function gemClick (gem) {
+//     console.log()
+//     userSum =+ gem;
+//     alert("gem clicked"+gem)
+//     console.log(userSum);
+//     checkStatus();
+//     console.log("gemclick")
+
+// }function gemClick (gem) {
+//     console.log()
+//     userSum =+ gem;
+//     alert("gem clicked"+gem)
+//     console.log(userSum);
+//     checkStatus();
+//     console.log("gemclick")
+// }
+
+
+//click events
+
+    $("#yellow").on("click",function(){
+        console.log('hi')
+if(yellowGem===0){
+    yellowGem = randomNum(1,12);
+    $("#yellow").text($(this).text());
+    console.log(yellowGem);
+}
+userSum+=yellowGem;
+$("#sum").text(userSum);
+    });
+
+
+      
+         
+
+
+
+
+
+
+
+ });
+  
+
+
+
+
+
+
+
+
 
 
 //when click on gem
